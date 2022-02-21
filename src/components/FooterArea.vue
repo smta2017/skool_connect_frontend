b3d3ac58067661a53511fbc09050e693d1b71fcb
ME <marwa.elshafei@gmail.com>
2022-02-21 11:44:22 +0200

    initial with style error

---
+++ src/components/FooterArea.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+                <footer class="footer-wrap-layout1">
+                    <div class="copyright">© Copyrights <a href="#">akkhor</a> 2019. All rights reserved. Designed by <a
+                            href="#">PsdBosS</a></div>
+                </footer>
+    </div>
+</template>
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'FooterArea',
+
+}
+</script>
+<style scoped>
+
+</style>
